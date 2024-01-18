9a6b94341b6cc3a44e3e952fb145b7ffc33467ee
Slobozhancky <il.slob93@gmail.com>
2024-01-18 21:23:25 +0200

    Lecture: added some permissions

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $permissions = config('permission.permissions');
+
+        foreach ($permissions as $resource){
+            foreach ($resource as $permission){
+                Permission::findOrCreate($permission);
+            }
+        }
+        //
+    }
+}
